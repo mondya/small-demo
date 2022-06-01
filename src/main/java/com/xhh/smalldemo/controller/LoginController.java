9f1b591f749944933476152a8c347999ee7fcf66
xhh <xhh19990210@gmail.com>
2022-06-01 22:09:15 +0800

    🚀 add

--- src/main/java/com/xhh/smalldemo/controller/LoginController.java
+++ src/main/java/com/xhh/smalldemo/controller/LoginController.java
@@ -6,6 +6,7 @@ import com.xhh.smalldemo.vo.common.ResultVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
 import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,24 +14,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/1.0/api")
 public class LoginController {
-    
+
     @Autowired
     UserService userService;
 
     @RequestMapping("/login")
     public ResultVO login(@RequestParam("mobile") String mobile,
-                          @RequestParam("password") String password) {
-        Assert.hasLength(mobile,"手机号不能为空");
-        Assert.hasLength(password,"密码不能为空");
+                          @RequestParam(value = "password", required = false) String password,
+                          @RequestParam(value = "checkNo", required = false) String checkNo) {
+        Assert.hasLength(mobile, "手机号不能为空");
+        if (StringUtils.hasLength(checkNo)){
+            // todo 查询验证码相关操作
+        }
         ResultVO resultVO = new ResultVO();
         User user = userService.getUserByMobileAndPWD(mobile, password);
-        if (!ObjectUtils.isEmpty(user)){
-            resultVO =  resultVO.success();
+        if (!ObjectUtils.isEmpty(user)) {
+            resultVO = resultVO.success();
         } else {
             resultVO.setStatus(0);
             resultVO.setMessage("登入失败，用户为空");
         }
-        
+
         return resultVO;
     }
 }
