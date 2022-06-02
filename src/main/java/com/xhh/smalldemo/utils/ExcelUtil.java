80c08bf213dd5a16dc892f032c15efdf7d7dd24a
xhh <xhh19990210@gmail.com>
2022-06-03 00:11:30 +0800

    add excelUtil

---
+++ src/main/java/com/xhh/smalldemo/utils/ExcelUtil.java
@@ -0,0 +1,21 @@
+package com.xhh.smalldemo.utils;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Map;
+
+//excel 处理
+public class ExcelUtil {
+    
+    public static XSSFSheet buildTitle(XSSFWorkbook workbook, Map<String, Object> map, String sheetName){
+        XSSFSheet sheet = workbook.createSheet(sheetName);
+        if (!CollectionUtils.isEmpty(map)){
+            map.forEach((String s, Object o) ->{
+                
+            });
+        }
+        return sheet;
+    }
+}
