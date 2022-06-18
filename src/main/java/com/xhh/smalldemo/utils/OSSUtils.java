d7cd842f77c9a8df43eebc2e61a38a01493fdae0
xhh <xhh19990210@gmail.com>
2022-06-18 20:35:41 +0800

    ossUtils 工具类

--- src/main/java/com/xhh/smalldemo/utils/OSSUtils.java
+++ src/main/java/com/xhh/smalldemo/utils/OSSUtils.java
@@ -16,17 +16,56 @@ import java.nio.file.Files;
  */
 @Slf4j
 public class OSSUtils {
-    //上传excel文件
-    void upload(String fileName, Workbook workbook){
-        
+
+    // 上传File文件
+    String upload(File file) {
+        FileInputStream fileInputStream = null;
+        String url = "";
+        try {
+            fileInputStream = new FileInputStream(file);
+            this.upload(fileInputStream, file.getName());
+        } catch (Exception e) {
+            log.error("文件写入失败");
+        } finally {
+            try {
+                if (fileInputStream != null){
+                    fileInputStream.close();
+                }
+            } catch (Exception e){
+                log.error("fileOutputStream关闭异常",e);
+            }
+        }
+        return url;
     }
-    
-    String upload(InputStream inputStream, String fileName){
+
+    //上传excel文件
+    String upload(String fileName, Workbook workbook) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        InputStream inputStream = null;
+        String url = "";
+        try {
+            workbook.write(outputStream);
+            inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+            url = upload(inputStream, fileName);
+        } catch (Exception e) {
+            log.error("workbook写入失败");
+        } finally {
+            try {
+                outputStream.close();
+            } catch (Exception e) {
+                log.error("outputStream关闭异常");
+            }
+        }
+
+        return url;
+    }
+
+    String upload(InputStream inputStream, String fileName) {
         OSS ossClient = new OSSClientBuilder().build(OSSConfig.END_POINT, OSSConfig.ACCESS_KEY_ID, OSSConfig.ACCESS_KEY_SECRET);
         try {
             ossClient.putObject(OSSConfig.BUCKET_NAME, fileName, inputStream);
-        } catch (Exception e){
-            log.error("上传阿里云失败,message:{}", e);
+        } catch (Exception e) {
+            log.error("上传阿里云失败,message:", e);
         } finally {
             ossClient.shutdown();
         }
