d13cddd6e9cd5777da24877f052f8799611b1bad
xhh <xhh19990210@gmail.com>
2022-06-19 17:25:46 +0800

    test class, for test🚀

---
+++ src/main/java/com/xhh/smalldemo/utils/Test.java
@@ -0,0 +1,20 @@
+package com.xhh.smalldemo.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        File file = new File("d:/108键盘.txt");
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+//        FileInputStream fileInputStream = new FileInputStream(file);
+        byte[] bytes = new byte[(int) file.length()];
+
+        fileOutputStream.write("bytes".getBytes());
+        System.out.println("bytes:" + Arrays.toString(bytes));
+        fileOutputStream.close();
+    }
+}
